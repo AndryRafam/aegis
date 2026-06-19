9953ca726d75e1824933a87891a53b00f2bcd12d
Andry Rafam <41973268+AndryRafam@users.noreply.github.com>
2026-06-19 17:31:01 +0300

    Update aes.hpp

--- src/simpleCipher/aes.hpp
+++ src/simpleCipher/aes.hpp
@@ -1,15 +1,19 @@
 #ifndef AES_H
 #define AES_H
 
+#include <fstream>
+#include <cstdio>
+#include <stdexcept>
+
 #include <cryptopp/cryptlib.h>
 #include <cryptopp/secblock.h>
 #include <cryptopp/aes.h>
 #include <cryptopp/gcm.h>
 #include <cryptopp/files.h>
 #include <cryptopp/osrng.h>
-#include <cryptopp/sha.h>
-#include <cryptopp/hkdf.h>
+#include <cryptopp/argon2.h>
 #include <cryptopp/hex.h>
+#include <cryptopp/filters.h>
 
 #include <string>
 
