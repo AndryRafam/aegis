746aad83dcd7bf5b65f798a3cd1052e6fa3c688f
Andry Rafam <41973268+AndryRafam@users.noreply.github.com>
2026-06-19 17:32:06 +0300

    Update sm4.hpp

--- src/simpleCipher/sm4.hpp
+++ src/simpleCipher/sm4.hpp
@@ -1,15 +1,19 @@
 #ifndef SM4_H
 #define SM4_H
 
+#include <fstream>
+#include <cstdio>
+#include <stdexcept>
+
 #include <cryptopp/cryptlib.h>
 #include <cryptopp/secblock.h>
 #include <cryptopp/sm4.h>
 #include <cryptopp/gcm.h>
 #include <cryptopp/files.h>
 #include <cryptopp/osrng.h>
-#include <cryptopp/sha.h>
-#include <cryptopp/hkdf.h>
+#include <cryptopp/argon2.h>
 #include <cryptopp/hex.h>
+#include <cryptopp/filters.h>
 
 #include <string>
 
