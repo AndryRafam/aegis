cf953b63585e7ff195c4cd9e5f0db8dad390c881
Andry Rafam <41973268+AndryRafam@users.noreply.github.com>
2026-06-19 17:33:02 +0300

    Update xchacha20Cipher.hpp

--- src/simpleCipher/xchacha20Cipher.hpp
+++ src/simpleCipher/xchacha20Cipher.hpp
@@ -1,14 +1,18 @@
 #ifndef XCHACHA20CIPHER_H
 #define XCHACHA20CIPHER_H
 
+#include <fstream>
+#include <cstdio>
+#include <stdexcept>
+
 #include <cryptopp/cryptlib.h>
 #include <cryptopp/secblock.h>
 #include <cryptopp/chachapoly.h>
 #include <cryptopp/files.h>
 #include <cryptopp/osrng.h>
-#include <cryptopp/sha.h>
-#include <cryptopp/hkdf.h>
+#include <cryptopp/argon2.h>
 #include <cryptopp/hex.h>
+#include <cryptopp/filters.h>
 
 #include <string>
 
